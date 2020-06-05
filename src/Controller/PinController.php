7e359d55c27d493715191d27b7b653759a7a2186
JgPhil <jamingph@gmail.com>
2020-06-05 14:30:34 +0200

    PinController

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -5,17 +5,53 @@ namespace App\Controller;
 use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinController extends AbstractController
 {
     /**
-     * @Route("/", name="pins")
+     * @Route("/", name="app_home", methods={"GET"})
      */
     public function index(PinRepository $repo): Response
     {
-        return $this->render('pins/index.html.twig', [ 'pins' => $repo->findAll()]);
+        return $this->render('pins/index.html.twig', [
+            'pins' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/pins/create", name="app_pin_create", methods={"GET", "POST"})
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response 
+    {
+        $form = $this->createFormBuilder()
+            ->add('title', TextType::class)
+            ->add('description', TextareaType::class)
+            ->getForm()
+            ;
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $pin = new Pin;
+            $pin->setTitle($data['title']);
+            $pin->setDescription($data['description']);
+
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+
+        return $this->render('pins/create.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
